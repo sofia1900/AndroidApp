59d51c94a8f7d8fc5a17c0da26277aa0d901c198
sofia <sofia.jimlap@educa.jcyl.es>
2023-10-09 20:36:28 +0200

    activity

--- app/src/main/java/com/iesam/fomapp/features/MainActivity.kt
+++ app/src/main/java/com/iesam/fomapp/features/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.iesam.fomapp.features
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.iesam.fomapp.R
+import com.iesam.fomapp.features.ex02.presentation.Ex02FormActivity
+import java.text.Normalizer.Form
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun setupView (){
+        findViewById<Button>(R.id.action_02Form).setOnClickListener {
+            startActivity(Intent(this, Ex02FormActivity::class.java))
+        }
     }
 }
