9031091c9bbe1e3512dac334d5cdba0c8a30a539
sofia1900 <sofia.jimlap@educa.jcyl.es>
2023-10-15 17:17:43 +0200

    Exercise 3 view 03

--- app/src/main/java/com/iesam/fomapp/features/ex03/ejem03/Ex03_3Activity.kt
+++ app/src/main/java/com/iesam/fomapp/features/ex03/ejem03/Ex03_3Activity.kt
@@ -7,6 +7,6 @@ import com.iesam.fomapp.R
 class Ex03_3Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ex033)
+        setContentView(R.layout.activity_ex03_3)
     }
 }
