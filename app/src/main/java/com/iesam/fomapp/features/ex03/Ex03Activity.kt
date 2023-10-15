a42c66245868bf1bf5db2e0a6c260929a7f3ab85
sofia1900 <sofia.jimlap@educa.jcyl.es>
2023-10-15 21:11:35 +0200

    Exercise 3 view 05

--- app/src/main/java/com/iesam/fomapp/features/ex03/Ex03Activity.kt
+++ app/src/main/java/com/iesam/fomapp/features/ex03/Ex03Activity.kt
@@ -9,6 +9,7 @@ import com.iesam.fomapp.features.ex03.ejem01.presentation.Ex03_1Activity
 import com.iesam.fomapp.features.ex03.ejem02.presentation.Ex03_2Activity
 import com.iesam.fomapp.features.ex03.ejem03.presentation.Ex03_3Activity
 import com.iesam.fomapp.features.ex03.ejem04.Ex03_4Activity
+import com.iesam.fomapp.features.ex03.ejem05.Ex03_5Activity
 
 class Ex03Activity : AppCompatActivity() {
 
@@ -33,6 +34,10 @@ class Ex03Activity : AppCompatActivity() {
 
         findViewById<Button>(R.id.action_3_4).setOnClickListener {
             startActivity(Intent(this, Ex03_4Activity::class.java))
+        }
+
+        findViewById<Button>(R.id.action_3_5).setOnClickListener {
+            startActivity(Intent(this, Ex03_5Activity::class.java))
         }
     }
 }
