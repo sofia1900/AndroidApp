1d4b832acc72070e9191ad1d4abd92902f07ee2c
sofia1900 <sofia.jimlap@educa.jcyl.es>
2023-10-21 01:47:55 +0200

    flujo huellas

--- app/src/main/java/com/iesam/fomapp/features/ex03/ejem06/presentation/Ex03_6Activity.kt
+++ app/src/main/java/com/iesam/fomapp/features/ex03/ejem06/presentation/Ex03_6Activity.kt
@@ -2,11 +2,48 @@ package com.iesam.fomapp.features.ex03.ejem06.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.iesam.fomapp.R
+import androidx.lifecycle.Observer
+import com.iesam.fomapp.databinding.ActivityEx036Binding
+import com.iesam.fomapp.features.ex03.ejem06.data.DogDataRepository
+import com.iesam.fomapp.features.ex03.ejem06.data.remote.ApiRemoteDataSource
+import com.iesam.fomapp.features.ex03.ejem06.domain.GetDogUseCase
+import com.iesam.fomapp.features.ex03.ejem06.data.local.XmlLocalDataSource
+import com.iesam.fomapp.features.ex03.ejem06.domain.Dog
 
 class Ex03_6Activity : AppCompatActivity() {
+
+    lateinit var binding: ActivityEx036Binding
+
+    private val viewModel : Ex03_6ViewModel by lazy {
+        Ex03_6ViewModel(GetDogUseCase(DogDataRepository(XmlLocalDataSource(this), ApiRemoteDataSource())))
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ex03_6)
+        bindView()
+        loadDog()
+    }
+
+    private fun bindView (){
+        binding = ActivityEx036Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    private fun loadDog(){
+        setupObserver()
+        viewModel.loadBurger()
+    }
+
+    private fun setupObserver () {
+        val observer = Observer<Ex03_6ViewModel.UiState>{
+            it.dog?.apply{
+                bindData(this)
+            }
+        }
+    }
+
+    private fun bindData(dog : Dog){
+        binding.apply {
+
+        }
     }
 }
